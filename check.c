fad0ae95d2dfe25e2113cc1ee4fef742045c5890
Kelian BOUCAUD <kboucaud@e1r3p18.42.fr>
2016-12-05 13:13:32 +0100

    Modify

--- check.c
+++ check.c
@@ -6,8 +6,22 @@
 /*   By: kboucaud <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2016/12/05 12:55:40 by kboucaud          #+#    #+#             */
-/*   Updated: 2016/12/05 13:00:55 by kboucaud         ###   ########.fr       */
+/*   Updated: 2016/12/05 13:02:59 by kboucaud         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "fillit.h"
+
+int		ft_check(const char *path)
+{
+	int		fd;
+	char	*file;
+
+	if ((file = (char*)malloc(sizeof(char) * BUFF_SIZE)) == NULL)
+		return (0);
+	fd = open(path, O_RDONLY);
+	if (fd == -1)
+		return (0);
+	if ((read(fd, file, BUFF_SIZE) == -1))
+		return (0);
+}
